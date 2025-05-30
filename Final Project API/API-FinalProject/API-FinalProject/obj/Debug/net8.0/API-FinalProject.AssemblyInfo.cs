b2ad1ca084f74855d8f6043ccddec11265aa6d73
aitajf <aytajrch@code.edu.az>
2025-05-30 10:20:19 +0400

    Eleven day: Product Edit method for API

--- Final Project API/API-FinalProject/API-FinalProject/obj/Debug/net8.0/API-FinalProject.AssemblyInfo.cs
+++ Final Project API/API-FinalProject/API-FinalProject/obj/Debug/net8.0/API-FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51abe5b2826ff890239732c8a2630428a8c0d820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a62a4dc02e2a90f9fb02ff0e95afcce72f78bef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Final Project API/API-FinalProject/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Final Project API/API-FinalProject/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51abe5b2826ff890239732c8a2630428a8c0d820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a62a4dc02e2a90f9fb02ff0e95afcce72f78bef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
